8c5a9f27bdc31f695430faf4003b2c7b8cb60ca2
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-03-06 15:47:17 -0500

    Update Dragon.ino to move top jaw
    changed for-loops to reflect correct position value adjustments

--- Dragon/src/Dragon.ino
+++ Dragon/src/Dragon.ino
@@ -11,7 +11,7 @@ void setup() {
  
 void loop() {
   // FOR-loop to open the dragon jaw:
-  for(pos = 0; pos <= -30; pos -= 1)
+  for(pos = 0; pos <= 30; pos += 1)
   {
     dragonServo.write(pos);   // Change the position of the servo arm to new position
     delay(5);             	// 5ms delay before next adjustment
@@ -19,7 +19,7 @@ void loop() {
   delay(4000);            	// delay for dragon roar/effects
  
   // FOR-loop to close the dragon jaw:
-  for(pos = -30; pos >= 0; pos += 1)
+  for(pos = 30; pos >= 0; pos -= 1)
   {
     dragonServo.write(pos);   // Change the position of the servo arm to new position
     delay(5);             	// 5ms delay before next adjustment
