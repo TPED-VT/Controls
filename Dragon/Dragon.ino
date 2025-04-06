d8f156968a81b007036577dac6e5e63ea59ca970
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-04-06 12:55:56 -0400

    Latest change to Dragon.ino to properly check serial characters

--- Dragon/Dragon.ino
+++ Dragon/Dragon.ino
@@ -1,13 +1,16 @@
 #include <Servo.h>
 
 Servo dragonServo;                            // Initialize dragon servo object
-int pos = 0;                                  // Initialize servo position
+int pos = 90;                                  // Initialize servo position
 int lit = 0;                                  // Initialize eye LEDs brightness
 int lava = 0;                                 // Initialize lava LED strip brightness
-bool active = false;                          // Make sure the dragon can run
+char send = 0;
+boolean active = false;                          // Make sure the dragon can run
 #define RED_PIN 5                             // Define the arduino PWM pin for the lava LED strip
 #define EYES_PIN 11                           // Define the arduino PWM pin for the eye LEDs
 #define JAW_PIN 9                             // Define the arduino pin for the servo
+#define HOME 90
+#define OPEN HOME+60
 
 // Hardware setup
 void setup() {
@@ -16,9 +19,11 @@ void setup() {
   active = false;
   // LED strip RGB setup code
   pinMode(RED_PIN, OUTPUT);                   // digital PWM pin 5
+  analogWrite(RED_PIN, 0);
 
   // Eye LEDs setup code
   pinMode(EYES_PIN, OUTPUT);                  // digital PWM pin 11
+  analogWrite(EYES_PIN, 0);
   
   // Servo setup code
   dragonServo.attach(JAW_PIN);                // digital pin 9
@@ -26,220 +31,266 @@ void setup() {
 }
 
 // Main loop to repeat continuously
-void loop() {
-  if(Serial.available() > 0)                  // Check for serial start from Raspberry Pi
+void loop() { 
+  while(Serial.available() > 0)                  // Check for serial start from Raspberry Pi
   {
-    char send = Serial.read();
-    if(send == 's')
+    send = Serial.read();
+    Serial.println(send);
+    if(char(send) == 's')
       active = false;
-    if(send == 'd' && !active)
+    if(char(send) == 'd' && !active)
       active = true;
     if(active)
     {
-      // FOR-loop to gradually turn lava LED strip on to full brightness
-      for(lava = 0; lava < 255; lava += 1)
-      {
-        analogWrite(RED_PIN, lava);             // Write new increased brightness to red pin of RGB LED strip (lava)
-        delay(5);                               // Wait 5ms before next loop iteration
-      }
-  
-      // 1.275 seconds
-      lavaLoop1();
-      lavaLoop2();
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      // Permanent Time: 5.875 seconds
-      // FOR-loop to gradually turn on dragon eyes to full brightness:
-      for(lit = 0; lit < 255; lit += 1)
-      {
-        analogWrite(EYES_PIN, lit);             // Write new increased brightness to dragon eyes pin
-        delay(5);                               // Wait 5ms before next loop iteration
-        if(lit % 100 == 0)
-          if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-    }
-  
-      // Permanent Time: 7.15 seconds
-      jawOpen();                                 // FOR-loop to open the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 7.45 seconds
-      lavaLoop1();  // 1.5 seconds
-      delay(50);    // 0.05 seconds
-      lavaLoop4();  // 0.5 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // Permanent Time: 9.5 seconds
-      jawClose();                                // FOR-loop to close the dragon jaw
-  
-      // 9.8 seconds
-      lavaLoop3();  // 2.8 seconds
-  
-      // Permanent Time: 12.6 seconds
-      jawOpen();                                 // FOR-loop to open the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 12.9 seconds
-      lavaLoop4();  // 0.5 seconds
-      delay(100);   // 0.1 seconds
-      lavaLoop2();  // 1.5 seconds
-      lavaLoop4();  // 0.5 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // Permanent Time: 15.5 seconds
-      jawClose();                                // FOR-loop to close the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 15.8 seconds
-      lavaLoop1();  // 3.1 seconds
-      lavaLoop2();  // 1.5 seconds
-      lavaLoop1();  // 3.1 seconds
-      lavaLoop4();  // 0.5 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 24 seconds
-      jawOpen();                                 // FOR-loop to open the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 24.3 seconds
-      lavaLoop3(); // 2.8 seconds
-      lavaLoop5(); // 0.45 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 27.55 seconds
-      jawClose();                                // FOR-loop to close the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 27.85 seconds
-      lavaLoop3();
-      lavaLoop1();
-      lavaLoop2();
-      lavaLoop1();
-      lavaLoop3();
-      lavaLoop1();
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 41.05 seconds
-      jawOpen();                                 // FOR-loop to open the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 41.35 seconds
-      lavaLoop1();  // 1.5 seconds
-      lavaLoop1();  // 1.5 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 44.35 seconds
-      jawClose();                                // FOR-loop to close the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 44.65 seconds
-      lavaLoop4();
-      lavaLoop2();
-      delay(50);
-      lavaLoop3();
-      lavaLoop4();
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-  
-      // 53.85 seconds
-      jawOpen();                                 // FOR-loop to open the dragon jaw
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // 54.15 seconds
-      lavaLoop1();    // 1.5 seconds
-      lavaLoop2();    // 3.1 seconds
-      lavaLoop4();    // 0.5 seconds
-      delay(75);      // 0.075 seconds
-      lavaLoop1();    // 1.5 seconds
-      lavaLoop2();    // 3.1 seconds
-      lavaLoop3();    // 2.8 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // FOR-loop to gradually turn off dragon eyes:
-      for(lit = 255; lit > 0; lit -= 1)
-      {
-        analogWrite(EYES_PIN, lit);             // Write new decreased brightness to dragon eyes pin
-        delay(5);                               // Wait 5ms before next loop iteration
-      }
- 
-      jawClose();                               // FOR-loop to close the dragon jaw
-  
-      // Permanent Time (Ride cycle ends): 66.2 seconds
-      lavaLoop2();    // 3.1 seconds
-      lavaLoop3();    // 2.8 seconds
-      lavaLoop1();    // 1.5 seconds
-      delay(225);     // 0.225 seconds
-      lavaLoop2();    // 3.1 seconds
-      lavaLoop3();    // 2.8 seconds
-      lavaLoop2();    // 3.1 seconds
-      lavaLoop3();    // 2.8 seconds
-      lavaLoop2();    // 3.1 seconds
-      if(Serial.read() == 's') {
-        softReset();
-        return;  // Check for a stop signal
-      }
-      
-      // FOR-loop to gradually turn off dragon eyes:
-      for(lava = 255; lava > 0; lava -= 1)
-      {
-        analogWrite(RED_PIN, lava);             // Write new decreased brightness to dragon eyes pin
-        delay(5);                               // Wait 5ms before next loop iteration
-      }
-  
-      // Total time: 90 seconds
+      showProfile();
     }
   }
+}
+
+void showProfile()
+{
+  // FOR-loop to gradually turn lava LED strip on to full brightness
+  for(lava = 0; lava < 255; lava += 1)
+  {
+    analogWrite(RED_PIN, lava);             // Write new increased brightness to red pin of RGB LED strip (lava)
+    delay(5);                               // Wait 5ms before next loop iteration
+  }
+  
+  // 1.275 seconds
+  lavaLoop1();
+  lavaLoop2();
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+  // Permanent Time: 5.875 seconds
+  // FOR-loop to gradually turn on dragon eyes to full brightness:
+  for(lit = 0; lit < 255; lit += 1)
+  {
+    analogWrite(EYES_PIN, lit);             // Write new increased brightness to dragon eyes pin
+    delay(5);                               // Wait 5ms before next loop iteration
+    if(lit % 100 == 0)
+    {
+      send = Serial.read();
+      if(char(send) == 's') 
+      {
+        softReset();
+        return;  // Check for a stop signal
+      }
+    }
+  }
+  
+  // Permanent Time: 7.15 seconds
+  jawOpen();                                 // FOR-loop to open the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 7.45 seconds
+  lavaLoop1();  // 1.5 seconds
+  delay(50);    // 0.05 seconds
+  lavaLoop4();  // 0.5 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // Permanent Time: 9.5 seconds
+  jawClose();                                // FOR-loop to close the dragon jaw
+  
+  // 9.8 seconds
+  lavaLoop3();  // 2.8 seconds
+  
+  // Permanent Time: 12.6 seconds
+  jawOpen();                                 // FOR-loop to open the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 12.9 seconds
+  lavaLoop4();  // 0.5 seconds
+  delay(100);   // 0.1 seconds
+  lavaLoop2();  // 1.5 seconds
+  lavaLoop4();  // 0.5 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // Permanent Time: 15.5 seconds
+  jawClose();                                // FOR-loop to close the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 15.8 seconds
+  lavaLoop1();  // 3.1 seconds
+  lavaLoop2();  // 1.5 seconds
+  lavaLoop1();  // 3.1 seconds
+  lavaLoop4();  // 0.5 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 24 seconds
+  jawOpen();                                 // FOR-loop to open the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 24.3 seconds
+  lavaLoop3(); // 2.8 seconds
+  lavaLoop5(); // 0.45 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 27.55 seconds
+  jawClose();                                // FOR-loop to close the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 27.85 seconds
+  lavaLoop3();
+  lavaLoop1();
+  lavaLoop2();
+  lavaLoop1();
+  lavaLoop3();
+  lavaLoop1();
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 41.05 seconds
+  jawOpen();                                 // FOR-loop to open the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 41.35 seconds
+  lavaLoop1();  // 1.5 seconds
+  lavaLoop1();  // 1.5 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 44.35 seconds
+  jawClose();                                // FOR-loop to close the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 44.65 seconds
+  lavaLoop4();
+  lavaLoop2();
+  delay(50);
+  lavaLoop3();
+  lavaLoop4();
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+  
+  // 53.85 seconds
+  jawOpen();                                 // FOR-loop to open the dragon jaw
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // 54.15 seconds
+  lavaLoop1();    // 1.5 seconds
+  lavaLoop2();    // 3.1 seconds
+  lavaLoop4();    // 0.5 seconds
+  delay(75);      // 0.075 seconds
+  lavaLoop1();    // 1.5 seconds
+  lavaLoop2();    // 3.1 seconds
+  lavaLoop3();    // 2.8 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // FOR-loop to gradually turn off dragon eyes:
+  for(lit = 255; lit > 0; lit -= 1)
+  {
+    analogWrite(EYES_PIN, lit);             // Write new decreased brightness to dragon eyes pin
+    delay(5);                               // Wait 5ms before next loop iteration
+  }
+ 
+  jawClose();                               // FOR-loop to close the dragon jaw
+  
+  // Permanent Time (Ride cycle ends): 66.2 seconds
+  lavaLoop2();    // 3.1 seconds
+  lavaLoop3();    // 2.8 seconds
+  lavaLoop1();    // 1.5 seconds
+  delay(225);     // 0.225 seconds
+  lavaLoop2();    // 3.1 seconds
+  lavaLoop3();    // 2.8 seconds
+  lavaLoop2();    // 3.1 seconds
+  lavaLoop3();    // 2.8 seconds
+  lavaLoop2();    // 3.1 seconds
+  send = Serial.read();
+  if(char(send) == 's') 
+  {
+    softReset();
+    return;  // Check for a stop signal
+  }
+      
+  // FOR-loop to gradually turn off lava:
+  for(lava = 255; lava > 0; lava -= 1)
+  {
+    analogWrite(RED_PIN, lava);             // Write new decreased brightness to dragon eyes pin
+    delay(5);                               // Wait 5ms before next loop iteration
+  }
+  
+  // Total time: 90 seconds
 }
 
 
@@ -247,7 +298,7 @@ void loop() {
 void jawOpen()
 {
   // FOR-loop to open the dragon jaw:
-  for(pos = 0; pos <= 30; pos += 1)
+  for(pos = HOME; pos <= OPEN; pos += 1)
   {
     dragonServo.write(pos);                 // Change the position of the servo arm to new position
     delay(10);                              // 10ms delay before next adjustment
@@ -257,7 +308,7 @@ void jawOpen()
 void jawClose()
 {
   // FOR-loop to close the dragon jaw:
-  for(pos = 30; pos >= 0; pos -= 1)
+  for(pos = OPEN; pos >= HOME; pos -= 1)
   {
     dragonServo.write(pos);                 // Change the position of the servo arm to new position
     delay(10);                              // 10ms delay before next adjustment
